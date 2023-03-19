184a2c031afd34bbbf9d5377bb61d3851809ae94
vgpur <vgpur@LAPTOP-HR52MGN3>
2023-03-19 20:18:52 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using ATIVIDADE04AED;
+using System;
+using System.Globalization;
+
+namespace ATIVIDADE04AED
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+
+            Console.WriteLine("Digite o valor do lado A do triângulo: ");
+            triangulo.LadoA = double.Parse(Console.ReadLine());
+            if (triangulo.LadoA <= 0)
+            {
+                while(triangulo.LadoA <= 0) 
+                {
+                    Console.WriteLine("Valor inválido! Digite um novo valor maior do que 0:");
+                    triangulo.LadoA = double.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Digite o valor do lado B do triângulo: ");
+            triangulo.LadoB = double.Parse(Console.ReadLine());
+            if(triangulo.LadoB <= 0)
+            {
+                while(triangulo.LadoB <= 0)
+                {
+                    Console.WriteLine("Valor inválido! Digite um novo valor maior do que 0: ");
+                    triangulo.LadoB = double.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Digite o valor do lado C do triângulo: ");
+            triangulo.LadoC = double.Parse(Console.ReadLine());
+            if(triangulo.LadoC <= 0)
+            {
+                while(triangulo.LadoC <= 0)
+                {
+                    Console.WriteLine("Valor inválido!  Digite um novo valor maior do que 0: ");
+                    triangulo.LadoC = double.Parse(Console.ReadLine());
+                }
+            }
+            triangulo.imprimirDados();
+
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATIVIDADE04AED
+{
+    internal class Triangulo
+    {
+        private double ladoA;
+        private double ladoB;
+        private double ladoC;
+
+
+        public double LadoA
+        {
+            get
+            {
+                return ladoA;
+            }
+            set
+            {
+                ladoA = value;
+            }
+        }
+        public double LadoB
+        {
+            get { return ladoB; }
+            set { ladoB = value; }
+        }
+
+        public double LadoC
+        {
+            get { return ladoC; }
+            set { ladoC = value; }
+        }
+        private string tipoTriangulo(double ladoA, double ladoB, double ladoC)
+        {
+            if (ladoA >= ladoB + ladoC)
+            {
+                return "NÃO FORMA TRIÂNGULO";
+            }
+            else if ((ladoA * ladoA) == (ladoB * ladoB) + (ladoC * ladoC))
+            {
+                return "TRINGULO RETANGULO";
+            }
+            else if ((ladoA * ladoA) > (ladoB * ladoB) + (ladoC * ladoC))
+            {
+                return "TRIÂNGULO OBTUSANGULO";
+            }
+            else if ((ladoA * ladoA) < ((ladoB * ladoB) + (ladoC * ladoC)))
+            {
+                return "TRIÂNGULO ACUTANGULO";
+            }
+            else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+            {
+                return "TRIÂNGULO EQUILATERO";
+            }
+            else
+            {
+                return "TRIÂNGULO ESCALENO";
+            }
+        }
+
+        public void imprimirDados()
+        {
+            Console.WriteLine("TIPO DO TRIÂNGULO: " + tipoTriangulo(LadoA, LadoB, LadoC));
+        }
+
+    }
+}
